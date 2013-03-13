1977b0d08c00dea4691e15ca2b0eb5b7e58893e1
Everett Griffiths <everett@fireproofsocks.com>
2013-03-12 21:56:51 -0600

    Most of it working...

--- installmodx.php
+++ installmodx.php
@@ -58,6 +58,60 @@ function abort($msg) {
 }
 
 /**
+ * How to function
+ */
+function show_help() {
+
+	print "
+----------------------------------------------
+MODX Installer Utility
+----------------------------------------------
+This utility is designed to let you quickly install MODX Revolution (http://modx.com/) 
+to your server via the command-line. 
+
+----------------------------------------------
+PARAMETERS:
+----------------------------------------------
+--config : path to an XML file, containing site info. See http://bit.ly/pvVcHw
+--zip : a MODX zip file, downloaded from ".DOWNLOAD_PAGE."
+--target : the root of your MODX install, relative to the current dir. E.g. public_html/
+--help : displays this help page.
+
+----------------------------------------------
+USAGE EXAMPLES:
+----------------------------------------------
+php ".basename(__FILE__)."
+
+    This is the most basic invocation. The user will be prompted for all info.
+
+php ".basename(__FILE__)." --zip=modx-2.2.5-pl
+
+    --zip tells the script to extract an existing local zip file instead of
+    downloading a new one. Path is relative.
+
+php ".basename(__FILE__)." --config=myconfig.xml
+
+	The --config option specifies a MODX XML configuration.  This file contains 
+	your database login, your MODX username, and other important data required 
+	to install MODX.  This file will be copied to setup/config.xml. If you are 
+	doing a lot of installs, keep a copy of an XML config file. Path is relative.
+
+php ".basename(__FILE__)." --target=public_html
+
+	The --target option specifies where to deploy MODX. No intermediate
+	directories will be created: the contents of the zip file will go to the 
+	target. Path is relative.
+
+----------------------------------------------
+BUGS and FEATURE SUGGESTIONS
+----------------------------------------------
+Please direct feedback about this script to https://github.com/craftsmancoding/modx_utils
+
+";
+	exit;
+}
+
+/**
  * Strip the front off the dir name to make for cleaner zipfile extraction.
  * Converts something like myzipdir/path/to/file.txt
  * to path/to/file.txt
@@ -98,7 +152,8 @@ function preflight() {
  */
 function print_banner() {
 	printf( "%c[2J", ESC ); //clear screen
-	print " .----------------.  .----------------.  .----------------.  .----------------. 
+	print "
+ .----------------.  .----------------.  .----------------.  .----------------. 
 | .--------------. || .--------------. || .--------------. || .--------------. |
 | | ____    ____ | || |     ____     | || |  ________    | || |  ____  ____  | |
 | ||_   \  /   _|| || |   .'    `.   | || | |_   ___ `.  | || | |_  _||_  _| | |
@@ -110,11 +165,11 @@ function print_banner() {
 | '--------------' || '--------------' || '--------------' || '--------------' |
  '----------------'  '----------------'  '----------------'  '----------------
 
-,--.                ,--.          ,--.,--.               
-|  |,--,--,  ,---.,-'  '-. ,--,--.|  ||  | ,---. ,--.--. 
-|  ||      \(  .-''-.  .-'' ,-.  ||  ||  || .-. :|  .--' 
-|  ||  ||  |.-'  `) |  |  \ '-'  ||  ||  |\   --.|  |    
-`--'`--''--'`----'  `--'   `--`--'`--'`--' `----'`--'    
+            ,--.                ,--.          ,--.,--.               
+            |  |,--,--,  ,---.,-'  '-. ,--,--.|  ||  | ,---. ,--.--. 
+            |  ||      \(  .-''-.  .-'' ,-.  ||  ||  || .-. :|  .--' 
+            |  ||  ||  |.-'  `) |  |  \ '-'  ||  ||  |\   --.|  |    
+            `--'`--''--'`----'  `--'   `--`--'`--'`--' `----'`--'    
                                                          
 ";
 	print 'Version '.THIS_VERSION . str_repeat(' ', 15).'by '. THIS_AUTHOR . PHP_EOL;
@@ -130,16 +185,23 @@ function get_args() {
 	$shortopts .= 'c::'; // Optional value
 	$shortopts .= 'z::'; // Optional value
 	$shortopts .= 't::'; // Optional value
+	$shortopts .= 'h::'; // Optional value
 	
 	$longopts  = array(
 	    'config::',    // Optional value
 	    'zip::',    // Optional value
 	    'target::',    // Optional value
+	   	'help::',    // Optional value
 	);
+	
 	$opts = getopt($shortopts, $longopts);
 	
+	if (isset($opts['help'])) {
+		show_help();
+	}
+	
 	if (isset($opts['config']) && !file_exists($opts['config'])) {
-		abort('Configuration file not found. ' . $opts['config']);
+		abort('XML configuration file not found. ' . $opts['config']);
 	}
 	else {
 		$opts['config'] = false;
@@ -147,12 +209,7 @@ function get_args() {
 	if (isset($opts['zip']) && !file_exists($opts['zip'])) {
 		abort('Zip file not found. ' . $opts['zip']);
 	}
-	if (isset($opts['target'])) {
-		if (file_exists($opts['target'])) {
-			abort('The target directory cannot already exist: '.$opts['target']);
-		}
-	}
-	else {
+	if (!isset($opts['target'])) {
 		$opts['target'] = null;
 	}
 
@@ -217,65 +274,19 @@ function download_modx($modx_zip) {
 	fclose($fp);	
 }
 
-
 /**
- * Extract the zip file.
- *
- * @param string $zipfile (relative to this script, e.g. myfile.zip)
- * @param string $extractto path where we want to extrat to
- */
-/*
-function extract_zip($zipfile,$extractto) {
-	print "Extracting to $extractto". PHP_EOL;
-	$zip = new ZipArchive;
-	if ($zip->open($zipfile) === true) {
-
-		//$zip->extractTo($extractto); // This did not work -- if there is a single error, it borks.
-		// The "." file generates a warning (wtf?)
-		// So we go through item by item.
-		for($i = 0; $i < $zip->numFiles; $i++) {
-			
-//			// This works, but you always end up having a parent $extractto dir
-//	        if(!@$zip->extractTo($extractto, array($zip->getNameIndex($i)))) {
-//	        	print 'Could not extract file '.$zip->getNameIndex($i).PHP_EOL;
-//	        }
-//	        else {
-//	        	print $zip->getNameIndex($i).PHP_EOL;
-//	        }
-//
-	        $filename = $zip->getNameIndex($i);
-
-			print $filename . PHP_EOL;			
-//			copy("zip://".$zipfile."#".$filename, $extractto.'x.txt');
-        	$dir = pathinfo($filename,PATHINFO_DIRNAME);
-        	if (!file_exists($dir)) {
-        		print 'Creating dir '.$dir .PHP_EOL;
-        		@mkdir($dir,0777,true);
-        	}
-//        	print "zip://".$zipfile."#".$filename.', '. $extractto.$filename . PHP_EOL;
-        	copy("zip://".$zipfile."#".$filename, $extractto.$filename);
-	                    
-	    }
-
-
-		$zip->close();
-		print "Extracted zip file $zipfile to $extractto".PHP_EOL;
-	} 
-	else {
-		abort('Could not open zip file' . $zipfile);
-	}
-}
-*/
-/**
+ * ZipArchive::extractTo did not do what I wanted, and it had errors. Boo.
  * The trick is to shift the "modx-2.2.6-pl" off from the front of the 
  * extraction. Instead of extracting to public_html/modx-2.2.6-pl/ we want
  * to extract straight to public_html/
+ * I couldn't find any other examples that did quite what I wanted.
  *
  * See http://stackoverflow.com/questions/5256551/unzip-the-file-using-php-collapses-the-zip-file-into-one-folder
+ *
+ * @param string $zipfile (relative to this script, e.g. myfile.zip)
+ * @param string $target path where we want to setup MODX, e.g. public_html/
  */
-function extract_zip($zipfile,$extractto) {
-	
-	$extractto = basename($extractto).DIRECTORY_SEPARATOR; // make sure we have a trailing slash.
+function extract_zip($zipfile,$target) {
 	
 	$z = zip_open($zipfile) or die("can't open $zipfile: $php_errormsg");
 	while ($entry = zip_read($z)) {
@@ -285,7 +296,7 @@ function extract_zip($zipfile,$extractto) {
 		// only proceed if the file is not 0 bytes long
 		if (zip_entry_filesize($entry)) {
 			// Put this in our own directory
-			$entry_name = $extractto . strip_first_dir($entry_name);
+			$entry_name = $target . strip_first_dir($entry_name);
 			print 'inflating: '. $entry_name .PHP_EOL;
 			$dir = dirname($entry_name);
 			// make all necessary directories in the file's path
@@ -304,25 +315,154 @@ function extract_zip($zipfile,$extractto) {
 					fclose($fh) or error_log("can't close: $php_errormsg");
 				} 
 				else {
-					error_log("can't open $dir/$file: $php_errormsg");
+					print "Can't open $dir/$file".PHP_EOL;
 				}
 				zip_entry_close($entry);
 			} 
 			else {
-				error_log("can't open entry $entry_name: $php_errormsg");
+				print "Can't open entry $entry_name" . PHP_EOL;
 			}
 		}
+	}
+	
+	print 'Extraction complete.'.PHP_EOL.PHP_EOL;
+}
+
+/**
+ * Prompt the user for the deets. Some of this we can detect already.
+ *
+ * @return array
+ */
+function get_data($data) {
+
+	print '-----------------------------------------' . PHP_EOL;
+	print 'Provide your configuration details below.'.PHP_EOL;
+	print '(you can review this before you install).'.PHP_EOL;
+	print '-----------------------------------------' . PHP_EOL.PHP_EOL;
+	
+	foreach($data as $k => $v) {
+		$default_label = ''; // with [brackets]
+		if (!empty($v)) {
+			$default_label = " [$v]";
+		}
+		print $k . $default_label.': ';
+		
+		$input = trim(fgets(STDIN));
+		if (!empty($input)) {
+			$data[$k] = $input;
+		}
+		else {
+			$data[$k] = $v;
+		}
+	}
+
+	return $data;
+}
+
+/**
+ * Prints data so the user can review it.
+ * @param array $data
+ */
+function print_review_data($data) {
+	printf( "%c[2J", ESC ); //clear screen
+	print '-----------------------------------------' . PHP_EOL;
+	print 'Review your configuration details.'.PHP_EOL;
+	print '-----------------------------------------' . PHP_EOL.PHP_EOL;
+	
+	foreach ($data as $k => $v) {
+		printf( "%' -24s", $k.':');
+		print $v . PHP_EOL;
 	}
 }
 
 /**
- * Check to ensure the directory we think contains MODX actually
- * does appear to contain MODX
- * @param string $dir directory containing MODX.
+ * Get XML configuration file that MODX will recognize.
+ * This is streamlined for the most common options.
+ *
+ * @param array $data
  */
-function verify_modx_dir($dir) {
+function get_xml($data) {	
+	
+	// Write XML File
+	$xml = '
+<!--
+Configuration file for MODX Revolution
 
+Created by the modxinstaller.php script.
+https://github.com/craftsmancoding/modx_utils
+-->
+<modx>
+	<database_type>mysql</database_type>
+    <database_server>localhost</database_server>
+    <database>'.$data['Database Name'].'</database>
+    <database_user>'.$data['Database User'].'</database_user>
+    <database_password>'.$data['Database Password'].'</database_password>
+    <database_connection_charset>utf8</database_connection_charset>
+    <database_charset>utf8</database_charset>
+    <database_collation>utf8_general_ci</database_collation>
+    <table_prefix>modx_</table_prefix>
+    <https_port>443</https_port>
+    <http_host>localhost</http_host>
+    <cache_disabled>0</cache_disabled>
+
+    <!-- Set this to 1 if you are using MODX from Git or extracted it from the full MODX package to the server prior
+         to installation. -->
+    <inplace>0</inplace>
+    
+    <!-- Set this to 1 if you have manually extracted the core package from the file core/packages/core.transport.zip.
+         This will reduce the time it takes for the installation process on systems that do not allow the PHP time_limit
+         and Apache script execution time settings to be altered. -->
+    <unpacked>0</unpacked>
+
+    <!-- The language to install MODX for. This will set the default manager language to this. Use IANA codes. -->
+    <language>en</language>
+
+    <!-- Information for your administrator account -->
+    <cmsadmin>'.$data['MODX Admin Username'].'</cmsadmin>
+    <cmspassword>'.$data['MODX Admin Password'].'</cmspassword>
+    <cmsadminemail>'.$data['MODX Admin Email'].'</cmsadminemail>
+
+    <!-- Paths for your MODX core directory -->
+    <core_path>'.$data['core_path'].'</core_path>
+
+    <!-- Paths for the default contexts that are installed. -->
+    <context_mgr_path>'.$data['mgr_path'].'</context_mgr_path>
+    <context_mgr_url>'.$data['mgr_url'].'</context_mgr_url>
+    <context_connectors_path>'.$data['connectors_path'].'</context_connectors_path>
+    <context_connectors_url>'.$data['connectors_url'].'</context_connectors_url>
+    <context_web_path>'.$data['base_path'].'</context_web_path>
+    <context_web_url>'.$data['base_url'].'</context_web_url>
+
+    <!-- Whether or not to remove the setup/ directory after installation. -->
+    <remove_setup_directory>1</remove_setup_directory>
+</modx>';
+	
+	return $xml;
 }
+
+/**
+ * Write XML data to a file.
+ * @param string $contents
+ * @param string $xml_path where we write this
+ */
+function write_xml($contents,$xml_path) {
+	if($fh = @fopen($xml_path, 'w')) {
+		fwrite($fh, $contents);
+		fclose($fh);
+		
+		print 'config.xml file written at '.$xml_path.PHP_EOL;
+	}
+	else {
+		print 'There was a problem opening '.$xml_path.' for writing.'.PHP_EOL;
+		print 'You can paste the following contents into '.$xml_path.PHP_EOL;
+		print 'and then run:  php ./index.php --installmode=new'. PHP_EOL;
+		print 'or navigate to your site via a browser and do a normal installation.'.PHP_EOL.PHP_EOL;
+		print $contents;
+		print PHP_EOL.PHP_EOL;
+		exit;
+	}
+}
+
 
 //------------------------------------------------------------------------------
 //! Vars
@@ -336,11 +476,14 @@ $i = 0; // for spinner iterations
 //------------------------------------------------------------------------------
 // preflight: php version, is cli?, can we write to the local dir?, etc...
 preflight();
-// Some eye-candy...
-print_banner();
 
 // Read and validate any command-line arguments
 $args = get_args();
+
+// Some eye-candy...
+print_banner();
+
+
 
 // Skip downloading if we've already got a zip file
 if ($args['zip']) {
@@ -369,70 +512,75 @@ else {
 
 // Prompt the user for target
 if (!$args['target']) {
-//	$extractto = getcwd().DIRECTORY_SEPARATOR.pathinfo($args['zip'],PATHINFO_FILENAME)
-//		.DIRECTORY_SEPARATOR;
-	$extractto = pathinfo($args['zip'],PATHINFO_FILENAME).DIRECTORY_SEPARATOR;
-//	$extractto = getcwd().DIRECTORY_SEPARATOR;
-	print "Where should this be unzipped to? ($extractto) > ";
-	$extractto_path = fgets(STDIN);
-	$extractto_path = trim($extractto_path);
-	if (!empty($extractto_path)) {
-		$extractto = $extractto_path;
+	// Default
+	$args['target'] = pathinfo($args['zip'],PATHINFO_FILENAME).DIRECTORY_SEPARATOR;
+	print "Where should this be unzipped to? [$target] > ";
+	$target_path = trim(fgets(STDIN));
+	if (!empty($target_path)) {
+		$args['target'] = $target_path;
 	}
 }
-$zip = getcwd() . DIRECTORY_SEPARATOR.$args['zip'];
-extract_zip($args['zip'],$extractto);
 
-// Test Database Connection
+// make sure we have a trailing slash on the target dir
+$target = basename($args['target']).DIRECTORY_SEPARATOR; 
 
-// Write XML File
-$xml = '<modx>
-	<database_type>mysql</database_type>
-    <database_server>localhost</database_server>
-    <database>modx_modx</database>
-    <database_user>db_username</database_user>
-    <database_password>db_password</database_password>
-    <database_connection_charset>utf8</database_connection_charset>
-    <database_charset>utf8</database_charset>
-    <database_collation>utf8_general_ci</database_collation>
-    <table_prefix>modx_</table_prefix>
-    <https_port>443</https_port>
-    <http_host>localhost</http_host>
-    <cache_disabled>0</cache_disabled>
+extract_zip($args['zip'],$target);
 
-    <!-- Set this to 1 if you are using MODX from Git or extracted it from the full MODX package to the server prior
-         to installation. -->
-    <inplace>0</inplace>
-    
-    <!-- Set this to 1 if you have manually extracted the core package from the file core/packages/core.transport.zip.
-         This will reduce the time it takes for the installation process on systems that do not allow the PHP time_limit
-         and Apache script execution time settings to be altered. -->
-    <unpacked>0</unpacked>
+// We can skip a lot of stuff if the user supplied an XML config...
+// otherwise we have to ask them a bunch of stuff.
+// Yes, and we even have a GOTO statement.
+$xml_path = $target.'setup/config.xml';
 
-    <!-- The language to install MODX for. This will set the default manager language to this. Use IANA codes. -->
-    <language>en</language>
+if (!$args['config']) {	
 
-    <!-- Information for your administrator account -->
-    <cmsadmin>username</cmsadmin>
-    <cmspassword>password</cmspassword>
-    <cmsadminemail>email@address.com</cmsadminemail>
+	$data = array();
+	
+	// Put anything here that you want to prompt the user about.
+	// Put default values here.
+	$data['Database Name'] = '';
+	$data['Database User'] = '';
+	$data['Database Password'] = '';
+	
+	$data['MODX Admin Username'] = '';
+	$data['MODX Admin Password'] = '';
+	$data['MODX Admin Email'] = '';
+	
+	ENTERDATA:
+	$data = get_data($data);
+	print_review_data($data);
+	
+	print PHP_EOL. "Is this correct? [y/n] >";
+	$yn = strtolower(trim(fgets(STDIN)));
+	if ($yn == 'n') {
+		goto ENTERDATA; // yeah... GOTO mf'er!
+	}	
+	
+	// Some defaults here.
+	// TODO: allow for installation in sub-directory
+	$data['core_path'] = getcwd().DIRECTORY_SEPARATOR.$target.'core/';
+	
+	$data['mgr_path'] = getcwd().DIRECTORY_SEPARATOR.$target.'manager/';
+	$data['mgr_url'] = '/manager/';
+	
+	$data['connectors_path'] = getcwd().DIRECTORY_SEPARATOR.$target.'connectors/';
+	$data['connectors_url'] = '/connectors/';
+	
+	$data['base_path'] = getcwd().DIRECTORY_SEPARATOR.$target;
+	$data['base_url'] = '/';
 
-    <!-- Paths for your MODX core directory -->
-    <core_path>/www/modx/core/</core_path>
+	$xml = get_xml($data);
 
-    <!-- Paths for the default contexts that are installed. -->
-    <context_mgr_path>/www/modx/manager/</context_mgr_path>
-    <context_mgr_url>/modx/manager/</context_mgr_url>
-    <context_connectors_path>/www/modx/connectors/</context_connectors_path>
-    <context_connectors_url>/modx/connectors/</context_connectors_url>
-    <context_web_path>/www/modx/</context_web_path>
-    <context_web_url>/modx/</context_web_url>
+}
+else {
+	// Get XML from config file
+	$xml = file_get_contents($args['config']);
+}
 
-    <!-- Whether or not to remove the setup/ directory after installation. -->
-    <remove_setup_directory>1</remove_setup_directory>
-</modx>';
+write_xml($xml, $xml_path);
 
+// Test Database Connection?  We can't do this unless the user provided data.
+
+// Run install
 // php ./index.php --installmode=new --config=/path/to/config.xml
 
 /*EOF*/
-
